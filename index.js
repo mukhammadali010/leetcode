0299f5faea1974d62a62fdb615eff5aebc7c562c
mukhammadali010 <muhammadalinosirov34@gmail.com>
2023-09-15 02:29:22 +0500

    left right

--- index.js
+++ index.js
@@ -149,19 +149,100 @@
 
 //2824
 
-var countPairs = function (nums, target) {
-  let count = 0;
-  let result = [];
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] < target) {
-        result.push(nums[i]);
-        result.push(nums[j]);
-        count++
-      }
-    }
-  }
-  return count;
+// var countPairs = function (nums, target) {
+//   let count = 0;
+//   let result = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] < target) {
+//         result.push(nums[i]);
+//         result.push(nums[j]);
+//         count++
+//       }
+//     }
+//   }
+//   return count;
 
+// };
+// console.log(countPairs([-1, 1, 2, 3, 1], 2));
+
+//===========================================================
+
+//1480
+
+// var runningSum = function (nums) {
+//     let result = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         let sum = 0
+//         for (let j = 0; j < i; j++) {
+//             sum += nums[j]
+//         }
+//         result.push(sum)
+//     }
+//     return result
+// };
+// runningSum([1, 2, 3, 4]);
+
+//1365
+
+//===================================================
+
+// var smallerNumbersThanCurrent = function (nums) {
+//   let sum = []
+//   for (let i = 0; i < nums.length; i++) {
+//       let count = 0;
+//       for (let j = 0; j < nums.length; j++) {
+//         if(nums[i] > nums[j]){
+//             count++
+//         }
+//     }
+//     sum.push(count)
+//   }
+//   return sum
+// };
+// smallerNumbersThanCurrent([8, 1, 2, 2, 3]);
+
+//====================================================
+
+//2114
+
+
+// var mostWordsFound = function(sentences) {
+//     let max = sentences[0].split(" ").length
+//     for(let i = 1 ; i  < sentences.length; i++){
+//         if(max < sentences[i].split(" ").length){
+//             max = sentences[i].split(" ").length
+//         }
+//     }
+//     return max
+// };
+
+// mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"])
+
+//================================================================
+
+//2574
+
+var leftRightDifference = function(nums) {
+    let left = [0]
+    let right = [0]
+    let result = []
+    for(let i = 0; i < nums.length-1; i++){
+       
+        left.push(nums[i] + left.at(-1))
+    }
+
+    for(let i = nums.length-1; i > 0; i--){
+        right.unshift(nums[i]+right[0])
+    }
+    for(let i = 0 ; i < left.length; i++){
+        for(let j = 0; j < right.length; j++){
+            if(i == j){
+                let sum = Math.abs(left[i] - right[j])
+                result.push(sum)
+            }
+        }
+    }
+    return result
 };
-console.log(countPairs([-1, 1, 2, 3, 1], 2));
+leftRightDifference([10,4,8,3])
