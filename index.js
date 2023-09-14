a0ef7003b678d3f27e2bc11f84720c650355c520
mukhammadali010 <muhammadalinosirov34@gmail.com>
2023-09-14 08:47:44 +0500

    detect maxValue

--- index.js
+++ index.js
@@ -9,9 +9,8 @@
 //             b=p.split('')
 //             a.sort()
 //             b.sort()
-          
-           
-//             if (JSON.stringify(a) === JSON.stringify(b)){  
+
+//             if (JSON.stringify(a) === JSON.stringify(b)){
 //                 a = new Set(a)
 //                 mx = Math.max(a.size, mx)
 //             }
@@ -22,29 +21,24 @@
 //     return mx
 
 //  var lengthOfLongestSubstring = function (s) {
-    
 
-    //  var l = '';
-    // for(let i = 0; i <s.length; i++){
-        // for(let j=0; j < s.length; j ++){
-        //     if(s[i] !== s[j]){
-        //         l.push(s[i])
-        //     }
-        // }
+//  var l = '';
+// for(let i = 0; i <s.length; i++){
+// for(let j=0; j < s.length; j ++){
+//     if(s[i] !== s[j]){
+//         l.push(s[i])
+//     }
+// }
 //         l = s[i] + ''
-    
+
 //     }
 //     return l
 
 // };
 
-
 // console.log(lengthOfLongestSubstring('pwwkew'))
 // console.log(lengthOfLongestSubstring("bbbbb"))
-// console.log(lengthOfLongestSubstring("abcabcbb"))   
-
-
-
+// console.log(lengthOfLongestSubstring("abcabcbb"))
 
 // var isValid = function(arr) {
 //     arr = arr.split("")
@@ -63,30 +57,74 @@
 //     }
 // }
 
-var isValid = function(arr) {
-    arr = arr.split("")
-    if(arr.length === 1){
-        return false
-    }
-    for (let i=0; i<arr.length - 1; i+=2){ 
-            if(arr[i]==="{" && arr[i+1]==="}"){
-                arr.splice(i,1);
-                arr.splice(i,1);
-                i-=2;
-            }
-            if(arr[i]==="(" && arr[i + 1]===")"){
-                arr.splice(i,1);
-                arr.splice(i,1);
-                i-=2;
-            }
-            if(arr[i]==="[" && arr[i+1]==="]"){
-                arr.splice(i,1);
-                arr.splice(i,1);
-                i-=2;
-            }
- };
+// var isValid = function(arr) {
+//     arr = arr.split("")
+//     if(arr.length === 1){
+//         return false
+//     }
+//     for (let i=0; i<arr.length - 1; i+=2){
+//             if(arr[i]==="{" && arr[i+1]==="}"){
+//                 arr.splice(i,1);
+//                 arr.splice(i,1);
+//                 i-=2;
+//             }
+//             if(arr[i]==="(" && arr[i + 1]===")"){
+//                 arr.splice(i,1);
+//                 arr.splice(i,1);
+//                 i-=2;
+//             }
+//             if(arr[i]==="[" && arr[i+1]==="]"){
+//                 arr.splice(i,1);
+//                 arr.splice(i,1);
+//                 i-=2;
+//             }
+//  };
 
- console.log(arr)
- return arr.length === 0 
-}
-console.log( isValid('{[]}'));
+//  console.log(arr)
+//  return arr.length === 0
+// }
+// console.log( isValid('{[]}'));
+
+// var numIdenticalPairs = function(nums) {
+//     let count = 0
+//     let b = [...new Set(nums)]
+//     for(let i =0 ; i <= b.length; i++){
+//         for(let j =i+1 ; j < nums.length; j++){
+//              if(nums[i] === nums[j] ){
+//                 count++
+//              }
+//              console.log(nums);
+//         }
+//     }
+//     return count
+// };
+// console.log(numIdenticalPairs([1,1,1,1]));
+
+//=========================================================
+
+// 1672
+
+var maximumWealth = function (accounts) {
+  let array = [];
+  let max = 0;
+  for (let i = 0; i < accounts.length; i++) {
+    const sumOfElements = accounts[i].reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    array.push(sumOfElements);
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+};
+console.log(
+  maximumWealth([
+    [1, 5],
+    [7, 3],
+    [3, 5],
+  ])
+);
